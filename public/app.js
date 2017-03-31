c63824bbdf24556d9387ea4dd4bf3ebb43f46df1
John Daly <jpdaly@gmail.com>
2017-03-31 16:02:16 +0100

    add xhm request function

--- public/app.js
+++ public/app.js
@@ -1,5 +1,27 @@
-var app = function(){
-	console.log("App Loaded");
-}
+	var makeRequest = function(url, callback){
+		var request = new XMLHttpRequest();
+		request.open("GET", url);
+		request.onload = callback;
+		request.send();
+	}
 
-window.onload = app;
+	var requestComplete = function(){
+
+		if(this.status !== 200){
+			return;
+		}
+
+			console.log('request complete');
+
+		var jsonString = this.responseText;
+		var returnedObject = JSON.parse(jsonString);
+		return(returnedObject);
+	}
+
+
+	var app = function(){
+		var weatherUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&b319899fc6ddf6c46f1fdbbbb0b52185"
+		var weather = makeRequest(weatherUrl, requestComplete);
+	}
+
+	window.onload = app;
