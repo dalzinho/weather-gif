274b28df39553cb6b7da7fff849dc61d641cc3d4
John Daly <jpdaly@gmail.com>
2017-03-31 17:47:26 +0100

    weather related gif displays. having trouble getting search input to query weather api

--- public/app.js
+++ public/app.js
@@ -1,3 +1,6 @@
+	var weatherType = null;
+	var gifUrl = null;
+
 	var makeRequest = function(url, callback){
 		var request = new XMLHttpRequest();
 		request.open("GET", url);
@@ -5,34 +8,76 @@
 		request.send();
 	}
 
-	var requestComplete = function(){
+	var handleWeatherRequest = function(){
 
 		if(this.status !== 200){
 			return;
 		}
 
-			console.log('request complete');
+		var jsonString = this.responseText;
+		var returnedObject = JSON.parse(jsonString);
+		showWeatherInfo(returnedObject);
+
+		weatherType = returnedObject.weather[0].description;
+
+		gifUrl = "http://api.giphy.com/v1/gifs/search?q=" + weatherType.split(' ').join('+') + "&api_key=dc6zaTOxFJmzC";
+	}
+
+	var handleGifRequest = function(){
+				if(this.status !== 200){
+			return;
+		}
+
+		console.log('request complete');
 
 		var jsonString = this.responseText;
 		var returnedObject = JSON.parse(jsonString);
-		console.log(returnedObject);
-		showWeatherInfo(returnedObject);
-
-		var sometext = ".list.weather.description";
+		showGif(returnedObject);
 	}
 
 	var showWeatherInfo = function(weatherObject){
 		var h1 = document.querySelector('#cityname');
-		h1.innerText = weatherObject.city.name;
+		var p = document.querySelector('#weather-description')
+		h1.innerText = "";
+		h1.innerText = weatherObject.name;
+		p.innerText = "";
+		p.innerText = weatherObject.weather[0].description;
+
+	}
+
+	var showGif = function(gifObject){
+		var gifDiv = document.querySelector('#weather-gif');
+		var img = document.createElement('img');
+		var randomnumber = Math.floor((Math.random() * gifObject.data.length) + 1)
+		img.src= "";
+		img.src = gifObject.data[randomnumber].images.original.url;
+		gifDiv.appendChild(img);
+	}
+
+	var handlePress = function(event){
+			if(event.which==13){
+
+				console.log('scope of return press', searchInput);
+
+				var weatherUrl="http://api.openweathermap.org/data/2.5/weather?q=" + this.value + "&APIKEY=b319899fc6ddf6c46f1fdbbbb0b52185"
+				makeRequest(weatherUrl, handleWeatherRequest);
+				setTimeout(function(){
+			makeRequest(gifUrl, handleGifRequest)}, 1000);
+			}
 	}
 
 	var app = function(){
+		// var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=Glasgow,uk&APIKEY=b319899fc6ddf6c46f1fdbbbb0b52185"
+		// makeRequest(weatherUrl, handleWeatherRequest);
+		// setTimeout(function(){
+		// 	makeRequest(gifUrl, handleGifRequest)}, 1000);
 
-
-		var weatherUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APIKEY=b319899fc6ddf6c46f1fdbbbb0b52185"
-		var weather = makeRequest(weatherUrl, requestComplete);
-
-		console.log(weather);
+		var searchInput = document.querySelector('#search-input');
+		console.log('search input', searchInput);
+		searchInput.onkeypress = function(event){
+			handlePress(event);
+		}.bind(this);
+		//local storage last viewed location?
 	}
 
 	window.onload = app;
